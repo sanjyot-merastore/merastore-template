94ba8896348da73434b802c015f93815ca6bc6a9
Sanjyot Agrueddy <sanjyot.agureddy4@gmail.com>
2025-07-06 18:56:55 +0530

    Fix test case

---
+++ test/MeraStore.Services.Template.UnitTests/Features/Health/GetHealthQueryHandlerTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+
+using MeraStore.Services.Template.Application.Features.Health;
+using MeraStore.Services.Template.Common;
+
+namespace MeraStore.Services.Template.UnitTests.Features.Health
+{
+  public class GetHealthQueryHandlerTests
+  {
+    [Fact]
+    public async Task Handle_ShouldReturnHealthyResponse()
+    {
+      // Arrange
+      var handler = new GetHealthQueryHandler();
+      var query = new GetHealthQuery();
+
+      // Act
+      var result = await handler.Handle(query, CancellationToken.None);
+
+      // Assert
+      result.Should().NotBeNull();
+      result.Status.Should().Be("Healthy");
+      result.Service.Should().Be(KeyStore.ServiceName);
+    }
+  }
+}
